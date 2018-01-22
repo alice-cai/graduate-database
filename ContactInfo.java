7f2ab6a6718d6e9db9b46423206538ca3f0cc72e
hehe-eheh <35466853+hehe-eheh@users.noreply.github.com>
2018-01-22 14:57:54 -0500

    Update ContactInfo.java

--- ContactInfo.java
+++ ContactInfo.java
@@ -7,7 +7,7 @@ public class ContactInfo
 	private String email;
 	private String address;
 	private String person;
-   Scanner sc = new Scanner (System.in);
+	Scanner sc = new Scanner (System.in);
    
 	public ContactInfo(String ph, String e, String p, String a)
 	{
@@ -15,34 +15,34 @@ public class ContactInfo
 		email = e;
 		address = a;
 		person = p;
-	}
+	}//This is initilized from the Program Database class
 	public ContactInfo()
-   {
-      System.out.print ("Enter program phone number: ");
-      phone=sc.nextLine();
-      System.out.print ("Enter program Email: ");
-      email=sc.nextLine();
-      System.out.print ("Enter program address: ");
-      address=sc.nextLine();
-      System.out.print ("Enter contact person: ");
-      person=sc.nextLine();
-   }
-   
+	   {
+	      System.out.print ("Enter program phone number: ");
+	      phone=sc.nextLine();
+	      System.out.print ("Enter program Email: ");
+	      email=sc.nextLine();
+	      System.out.print ("Enter program address: ");
+	      address=sc.nextLine();
+	      System.out.print ("Enter contact person: ");
+	      person=sc.nextLine();
+	   }//This is initilized from admin's input
+
    public String getPhone()
    {
       return phone;
    }
-   
+
    public String getEmail()
    {
       return email;
    }
-   
+
    public String getPerson()
    {
       return person;
    }
-   
+
    public String getAddress()
    {
       return address;
@@ -55,12 +55,12 @@ public class ContactInfo
 		System.out.println(this);
 		System.out.print("Press any key to return to previous menu.");
 		input = sc.nextLine(); 
-	}
+	}//This is displayed once the user chooses to view the program
 	
 	public String toString ()
 	{
 		return("Phone: " + phone + "\nEmail: " + email + "\naddress: "+ address + "\nperson: " + person + "\n");
-	}
+	}//This is for displaying purposes
    
 	public void save (String file)
    {
@@ -77,6 +77,6 @@ public class ContactInfo
       {
          System.out.println("Problem saving contact information.");
       }
-   }
+   }//Saves contact information of a program
 	
 }
